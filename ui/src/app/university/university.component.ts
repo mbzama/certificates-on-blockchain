1c460b2f9432c550a120443d626e7970e1481b51
rahulrajput02 <rahul_king_may2@yahoo.com>
2018-07-31 16:31:51 +0530

    Refactored Code

--- ui/src/app/university/university.component.ts
+++ ui/src/app/university/university.component.ts
@@ -21,12 +21,6 @@ export class UniversityComponent {
   uncheckedData = [];
 
   angularForm = new FormGroup({
-    studentName: new FormControl(),
-    studentBranch: new FormControl(),
-    passingYear: new FormControl(),
-    collegeName: new FormControl(),
-    collegeId: new FormControl(),
-    studentEmail: new FormControl()
   });
 
   form = new FormGroup({
@@ -39,15 +33,6 @@ export class UniversityComponent {
   }
 
   createForm() {
-    this.angularForm = this.fb.group({
-      studentName: ['', Validators.required],
-      studentBranch: ['', Validators.required],
-      passingYear: ['', Validators.required],
-      collegeName: ['', Validators.required],
-      collegeId: ['', Validators.required],
-      studentEmail: ['', Validators.required],
-    });
-
     this.form = this.fb.group({
       login: ['', Validators.required],
       password: ['', Validators.required],
@@ -92,17 +77,9 @@ export class UniversityComponent {
     this.getVerifiedStudent();
   }
 
-  submitToBlockchain() {
-    this.submit = true;
-  }
-
   checkboxClicked(data) {
     this.checkedBoxes.push(data.ID);
     this.uncheckedData.push(data);
-  }
-
-  checkboxClickedForConfirmation(data) {
-    console.log(data);
   }
 
   sendDataForConfirmation() {
@@ -140,70 +117,5 @@ export class UniversityComponent {
           console.log("Error Ocurred" + err);
         }
       )
-  }
-
-
-  recordSubmission(event) {
-    const target = event.target;
-    const studentName = target.querySelector('#studentName').value;
-    const studentBranch = target.querySelector('#studentBranch').value;
-    const passingYear = target.querySelector('#passingYear').value;
-    const collegeName = target.querySelector('#collegeName').value;
-    const collegeId = target.querySelector('#collegeId').value;
-    const studentEmail = target.querySelector('#studentEmail').value;
-
-
-    console.log(studentName, studentBranch, passingYear, collegeName, collegeId, studentEmail);
-    this.angularForm.reset();
-
-
-    // const debtorState = target.querySelector('#debtorState').value;
-    // const debtorPostcode = target.querySelector('#debtorPostcode').value;
-    // const securedtype = target.querySelector('#securedtype').value;
-    // var securedPartyType;
-
-    // if (target.querySelector('#securedBusiness').checked) {
-    //   securedPartyType = target.querySelector('#securedBusiness').value;
-    // } else {
-    //   securedPartyType = target.querySelector('#securedindividual').value;
-    // }
-
-    // const securedOrgName = target.querySelector('#securedOrgName').value;
-    // const securedmaillingAddress = target.querySelector('#securedmaillingAddress').value;
-    // const securedCity = target.querySelector('#securedCity').value;
-    // const securedState = target.querySelector('#securedState').value;
-    // const securedPostcode = target.querySelector('#securedPostcode').value;
-    // const collateralType = target.querySelector('#collateralType').value;
-    // const attachmentType = target.querySelector('#attachmentType').value;
-    // var collateralIS;
-
-    // if (target.querySelector('#collateralIsNone').checked) {
-    //   collateralIS = target.querySelector('#securedBusiness').value;
-    // } else if (target.querySelector('#collateralIsTrust').checked) {
-    //   collateralIS = target.querySelector('#collateralIsTrust').value;
-    // } else {
-    //   collateralIS = target.querySelector('#collateralIsRep').value;
-    // }
-
-    // const myobj = {
-    //   "New_Filling_State": newFillingState, "New_Filling_Jurisdiction": newFillingJuidiction, "Filling_Form_Type": fillingFormType, "Student_Name": studentName, "Debtor_Type": debtorType, "Debtor_Party_type": debtorPartyType, "Debtor_Organisation_Name": debtorOrgName, "Debtor_Mailing_Address": debtorMaillingAddress, "Debtor_City": debtorCity, "Debtor_State": debtorState, "Debtor_Postal_Code": debtorPostcode,
-    //   "Secured_Party_Type": securedtype, "Party_Type": securedPartyType, "Secured_Party_Organisation_Name": securedOrgName, "Secured_Party_Mailing_Address": securedmaillingAddress, "Secured_Party_City": securedCity, "Secured_Party_State": securedState, "Secured_Party_Postal_Code": securedPostcode,
-    //   "Collateral_Type": collateralType, "Type_of_Attachment": attachmentType, "Collateral_Is": collateralIS
-    // };
-
-    // this.httpClient.post(environment.postNewFilling, myobj, {
-    //   responseType: 'text',
-    // })
-    //   .subscribe(
-    //     response => {
-    //       console.log(response);
-    //       this.formSubmitted = true;
-    //       this.angularForm.reset();
-
-    //     },
-    //     err => {
-    //       console.log("Error Ocurred" + err);
-    //     }
-    //   )
   }
 }
