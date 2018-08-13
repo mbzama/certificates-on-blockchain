f87c1f9e8952cbef962bd4be142bc9420a2ba7bb
Hari Giduturi <hgiduturi@evoketechnologies.com>
2018-08-13 19:02:49 +0530

    Added docker config

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  getUnverifiedStudentsData: 'http://localhost:3000/unverifiedstudents',
-  getVerifiedStudentsData: 'http://localhost:3000/verifiedstudents',
-  postStudentForConfirmation: 'http://localhost:3000/universityverify',
-  getCollegeStudents: 'http://localhost:3000/collegestudents',
-  getCollegeVerify: 'http://localhost:3000/collegeverify',
-  getStudent: 'http://localhost:3000/getStudent',
-  verifyStudent: 'http://localhost:3000/studentverify',
-  validate: 'http://localhost:3000/validate',
-  addStudent: 'http://localhost:3000/createcertificate'
+  getUnverifiedStudentsData: 'http://192.168.99.100:3000/unverifiedstudents',
+  getVerifiedStudentsData: 'http://192.168.99.100:3000/verifiedstudents',
+  postStudentForConfirmation: 'http://192.168.99.100:3000/universityverify',
+  getCollegeStudents: 'http://192.168.99.100:3000/collegestudents',
+  getCollegeVerify: 'http://192.168.99.100:3000/collegeverify',
+  getStudent: 'http://192.168.99.100:3000/getStudent',
+  verifyStudent: 'http://192.168.99.100:3000/studentverify',
+  validate: 'http://192.168.99.100:3000/validate',
+  addStudent: 'http://192.168.99.100:3000/createcertificate'
 };
