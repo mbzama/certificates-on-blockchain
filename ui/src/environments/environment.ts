b27619ad8c76390597b73c908fcd34427133575e
Blockchain Practice <bcpadmin@devops.evoketechnologies.com>
2018-08-17 06:43:03 +0000

    docekr config

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  getUnverifiedStudentsData: 'http://localhost:3000/unverifiedstudents',
-  getVerifiedStudentsData: 'http://localhost:3000/verifiedstudents',
-  postStudentForConfirmation: 'http://localhost:3000/universityverify',
-  getCollegeStudents: 'http://localhost:3000/collegestudents',
-  getCollegeVerify: 'http://localhost:3000/collegeverify',
-  getStudent: 'http://localhost:3000/getStudent',
-  verifyStudent: 'http://localhost:3000/studentverify',
-  validate: 'http://localhost:3000/validate',
-  addStudent: 'http://localhost:3000/createcertificate'
+  getUnverifiedStudentsData: 'http://52.172.13.43:3000/unverifiedstudents',
+  getVerifiedStudentsData: 'http://52.172.13.43:3000/verifiedstudents',
+  postStudentForConfirmation: 'http://52.172.13.43:3000/universityverify',
+  getCollegeStudents: 'http://52.172.13.43:3000/collegestudents',
+  getCollegeVerify: 'http://52.172.13.43:3000/collegeverify',
+  getStudent: 'http://52.172.13.43:3000/getStudent',
+  verifyStudent: 'http://52.172.13.43:3000/studentverify',
+  validate: 'http://52.172.13.43:3000/validate',
+  addStudent: 'http://52.172.13.43:3000/createcertificate'
 };
