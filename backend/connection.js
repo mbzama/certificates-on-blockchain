7b4e30bd81536acfc5ed1bd0da6ab29afc904137
Blockchain Practice <bcpadmin@devops.evoketechnologies.com>
2018-08-17 06:43:49 +0000

    docekr config

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '192.168.99.100',
+    host     : '52.172.13.43',
     user     : 'root',
     password : 'root',
     database : 'certificates'
