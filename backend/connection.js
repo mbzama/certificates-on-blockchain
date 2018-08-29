a15ce4d8bce7734071a0b9f6e870e9f5ab10dd2e
Hari <imhari213@gmail.com>
2018-08-29 13:10:41 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,16 +1,27 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+
+var connection      =   mysql.createPool({
     host     : '52.172.13.43',
     user     : 'root',
     password : 'root',
-    database : 'certificates'
+    database : 'certificates',
+    connectionLimit : 10,
+    debug    :  false
 });
 
-connection.connect(function(err) {
+
+connection.getConnection(function(err) {
     if (err) throw err;
     else 
     console.log("success");
+    connection.on('error', function(err) {      
+        if(err) throw err;
+        else
+        console.log("connectioon on");
+        return;     
+  });
 });
 
 module.exports = connection;
+
 
